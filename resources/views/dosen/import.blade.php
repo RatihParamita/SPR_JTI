9a2813f9e0b9cf9bf9bee3dc74450ba6a55168ea
Ratih Paramita <significantratih@gmail.com>
2025-12-18 22:31:45 +0700

    Membuat Fitur Impor Data Dosen (proses reload masih bermasalah)

---
+++ resources/views/dosen/import.blade.php
@@ -0,0 +1,36 @@
+<form action="{{ url('/dosen/import_ajax') }}" method="POST" id="form-import" enctype="multipart/form-data" onsubmit="importDosen(event)">
+    @csrf
+    <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" style="font-weight: bold;">Impor Data Dosen</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            <div>
+                Pastikan format file Anda sesuai dengan template yang disediakan.
+                <br>
+                <a href="{{ asset('templates/template_dosen.xlsx') }}" class="btn btn-sm btn-import" download>
+                    <i class="fas fa-download"></i> Unduh template Excel
+                </a>
+                <br>
+                <br>
+            </div>
+            <div class="form-group">
+                <label>Pilih File Excel:</label>
+                <input type="file" class="form-control" id="file_dosen" name="file_dosen" required>
+                <small id="error_file_dosen" class="form-text text-danger"></small>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                <i class="fas fa-times"></i> Batal
+            </button>
+            <button type="submit" class="btn btn-import">
+                <i class="fas fa-upload"></i> Impor
+            </button>
+        </div>
+    </div>
+</form>
+
