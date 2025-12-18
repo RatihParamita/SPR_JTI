4c11e851d01ab438e2d6798d3e1c908489eafdb6
Ratih Paramita <significantratih@gmail.com>
2025-12-18 22:32:44 +0700

    Pembagian View Dasbor Awal

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class WelcomeController extends Controller
 {
@@ -12,9 +15,34 @@ class WelcomeController extends Controller
             'title' => 'Selamat Datang!',
             'list' => ['Home', 'Dashboard']
         ];
-
+        $user = Auth::user();
+        $levelId = (int) ($user->level_id ?? 0);
         $activeMenu = 'dashboard';
 
-        return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+        // ====== Hitung statistik dasar ======
+        $today = now()->toDateString();
+
+        $totalRuangan   = DB::table('m_ruangan')->count();
+
+        $jadwalHariIni  = DB::table('t_jadwal')
+            ->whereDate('jadwal_tgl', $today)
+            ->count();
+        
+        
+
+        switch($user->level_id) {
+            case 1:
+                return view('admin.welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+            case 2:
+                return view('dosen.welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+            case 3:
+                return view('tendik.welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+            case 4:
+                return view('mahasiswa.welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+            
+
+        }
+
+        //return view('welcome', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
     }
 }
